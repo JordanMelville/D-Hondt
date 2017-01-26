0a76bf2c9349c1f2925b00dc8f2c63f5c4d9d6f2
Jordan <Jordan@Jordan-PC>
2017-01-26 19:26:08 +0000

    Final version of the prototype there was a massive issue earlier, after inspecting old emails from my supervisor, wanted to check to make sure everything worked before moving on and low and behold there was an issue still with last 5 seats allocation with my first sainte lague prototype throwing everything out of wack as this was adapted from the first. Everything is sorted(i hope)

---
+++ D'hondtMethodPrototype/src/Party.java
@@ -0,0 +1,39 @@
+
+public class Party {
+
+private String partyName;
+private int totalVotes;
+private int totalSeats;
+
+public Party(String name, int votes) {
+	totalVotes = votes;
+	partyName = name;
+	
+}
+
+public void addSeat() {
+	totalSeats++;
+}
+
+// formula for calculating the quotients
+public Double Quotient() {
+	return (double) totalVotes / (totalSeats + 1); 
+}
+
+public String partyName() {
+	return partyName;
+}
+
+public int totalVotes() {
+	return totalVotes;
+}
+
+public int totalSeats() {
+	return totalSeats;
+	
+}
+public String printPartyData() {
+	return partyName + " Votes " + Integer.toString(totalVotes) + " Seats " + Integer.toString(totalSeats);
+}
+
+}
